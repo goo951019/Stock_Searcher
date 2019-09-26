6fc06fa26837389c6c39393d856934a3f75c9cbf
goo951019 <goo1140@gmail.com>
2019-09-26 17:50:18 -0400

    HTML added and connected

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from flask import render_template
 from flask_api import FlaskAPI
 from flask_api import status
 from search.util.dataGrabber import grab_and_validate_symbol
@@ -7,13 +8,7 @@ app = FlaskAPI(__name__)
 
 @app.route('/')
 def root():
-    return ("Welcome to Stock Searcher!<br/>"
-            "To search stock please browse:<br/>"
-            "https://stock-searcher.appspot.com/Stock_Symbol<br/>"
-            "Example: https://stock-searcher.appspot.com/AMZN<br/>"
-            "<br/>"
-            "To Check status<br/>"
-            "https://stock-searcher.appspot.com/status")
+    return render_template('home.html')
 
 @app.route('/<string:stock_symbol>')
 def stock_api(stock_symbol):
@@ -23,8 +18,11 @@ def stock_api(stock_symbol):
 def status_check():
     if not grab_and_validate_symbol("AMZN"):
         return status.HTTP_404_NOT_FOUND
-    return "Server Running"
+    return render_template('status.html', status = 'Server Running')
 
 # Test line
 print(stock_API_Response('AMZN'))
 print("Project works!")
+
+if __name__ == "__main__":
+    app.run(debug=True)
